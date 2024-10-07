879f994b32ee827645f1c9e125c6980b80f6d561
webreflection <andrea.giammarchi@gmail.com>
2024-10-07 11:22:07 +0200

    Oooops, fixed typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function bytes() {
-  return new Uin8Array(await this.arrayBuffer());
+  return new Uint8Array(await this.arrayBuffer());
 }
 
 for (const C of ['Blob', 'Request', 'Response']) {
